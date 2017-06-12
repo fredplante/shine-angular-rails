02e11b1cce53ef6b778377aed4affb0098e377a6
Frédéric Planté <frederic.plante@yahoo.fr>
2017-06-12 16:53:06 +0200

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,13 @@
+User.create!(email: 'john.doe@example.com', password: 'password12',
+              password_confirmation: 'password12')
 
-if User.count.zero?
-  User.create!(email: 'john.doe@example.com', password: 'password12',
-                password_confirmation: 'password12')
-
-  5_000.times do |i|
-    Customer.create!(
-      first_name: Faker::Name.first_name,
-      last_name: Faker::Name.last_name,
-      username: "#{Faker::Internet.user_name}#{i}",
-      email: "#{Faker::Internet.user_name}#{i.to_s}@#{Faker::Internet.domain_name}"
-    )
-  end
+2_000.times do |i|
+  Customer.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    username: "#{Faker::Internet.user_name}#{i}",
+    email: "#{Faker::Internet.user_name}#{i.to_s}@#{Faker::Internet.domain_name}"
+  )
 end
 
 # Create all 50 states in the US
